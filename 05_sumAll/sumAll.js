f995485e072891e9eaf0688eddb86dac5c054f96
Jacob Byrne <25340202+Jacob2017@users.noreply.github.com>
2024-07-31 08:24:01 +0100

    test 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if ((typeof min !== "number") ||
+        (typeof max !== "number")) {
+        return "ERROR";
+    } else if (
+        (min < 0) || (max < 0) 
+    ) {
+        return "ERROR";
+    } else if (
+        (Math.round(min) !== min) ||
+        (Math.round(max) !== max)
+    ) {
+        return "ERROR";
+    } else if (min > max) {
+        return sumAll(max, min);
+    }
+
+    let sum = 0;
+    
+    for (i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum
 
 };
 
