679ec77d2f6dcfbbae5339797138578a8b5f8448
Jacob Byrne <25340202+Jacob2017@users.noreply.github.com>
2024-07-31 08:36:41 +0100

    test 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degF) {
+
+  let degC = (degF - 32) * (5 / 9);
+  return round(degC, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degC) {
+  let degF = (degC * (9/5)) + 32;
+  return round(degF, 1);
 };
+
+const round = function(value, precision) {
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
