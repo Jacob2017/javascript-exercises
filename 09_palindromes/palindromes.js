a04d8b25281ca064131b1a9cfada7dddce4102fd
Jacob Byrne <25340202+Jacob2017@users.noreply.github.com>
2024-08-06 10:12:14 +0100

    test 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanStr = str
+        .split("")
+        .map((letter) => letter.toLowerCase())
+        .filter((letter) => "abcdefghijklmnopqrstuvwxyz0123456789".includes(letter))
+        .join("");
+    
+    const reverseStr = cleanStr.split('').reverse().join('');
 
+    return cleanStr === reverseStr;
 };
 
 // Do not edit below this line
