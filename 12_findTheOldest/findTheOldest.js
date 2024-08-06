d481001803f29d39cc24174085e99b0746859516
Jacob Byrne <25340202+Jacob2017@users.noreply.github.com>
2024-08-06 13:13:38 +0100

    test 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let indexOfOldest = arr
+        .map((person) => {
+            let age = person.yearOfDeath - person.yearOfBirth;
+            if (isNaN(age)) {
+                age = 2024 - person.yearOfBirth;
+            }
+            return age;
+        })
+        .reduce((maxIndex, currentValue, currentIndex, arr) =>
+            currentValue > arr[maxIndex] ? currentIndex : maxIndex, 0);
+        
+    return arr[indexOfOldest];
 };
 
 // Do not edit below this line
