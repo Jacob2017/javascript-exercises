bb7d632fee0a7ef8f8699fe69109666aeb0f5765
Jacob Byrne <25340202+Jacob2017@users.noreply.github.com>
2024-08-06 13:13:57 +0100

    test 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (num, memory = {}) {
+    num = Number(num);
+    if (num < 0) {
+        return "OOPS";
+    } else if (num === 0) {
+        memory[num] = 0;
+        return 0;
+    } else if (num === 1) {
+        memory[num] = 1;
+        return 1;
+    } else if (num in memory) {
+        return memory[num];
+    }
+    memory[num] = fibonacci(num - 1, memory) + fibonacci(num - 2, memory);
+    // console.log(num, arr);
+    return fibonacci(num - 1, memory) + fibonacci(num - 2, memory);
 };
 
 // Do not edit below this line
